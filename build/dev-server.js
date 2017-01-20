3c784ab5b3013ffbef7ca09e4b5829c588239972
Junyong <zengjunyong@gmail.com>
2017-01-20 12:27:39 +0800

    log, just for test purpose

--- build/dev-server.js
+++ build/dev-server.js
@@ -30,6 +30,7 @@ var hotMiddleware = require('webpack-hot-middleware')(compiler, {
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    console.log('html-webpack-plugin-after-emit: reload');
     hotMiddleware.publish({ action: 'reload' })
     cb()
   })
